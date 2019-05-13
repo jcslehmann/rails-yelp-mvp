40927fe2a1d30607f748436601481df24b6dbbb4
Julius Lehmann <jcslehmann@gmail.com>
2019-05-13 15:58:05 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'China Tang',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '782182392',
+    category:  'chinese',
+  },
+  {
+    name:         'Yimonoki',
+    address:      'Ginza, Tokyo',
+    phone_number: '782182392',
+    category:  'japanese',
+  },
+  {
+    name:         'Paris Bistro',
+    address:      'Champs Elysees, Paris',
+    phone_number: '782182392',
+    category:  'french',
+  },
+  {
+    name:         'Mamma Mia Pizza',
+    address:      'Old Quarter, Rome',
+    phone_number: '782182392',
+    category:  'italian',
+  },
+  {
+    name:         'Chocolate Diner',
+    address:      'Old Quarter, Brussels',
+    phone_number: '782182392',
+    category:  'belgian',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
